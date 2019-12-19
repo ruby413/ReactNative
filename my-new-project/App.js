4e201d466f8f635b83a363588800c9d0ba54d9b0
juyeon_nmodelin <ruby413@hanmail.net>
2019-12-19 18:02:49 +0900

    nomad coder - react native, making weather app 1~14 class conplete

--- my-new-project/App.js
+++ my-new-project/App.js
@@ -1,21 +1,73 @@
-import React, {useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import Weather from "./Weather";
 
-export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!', )
-  return (
-    <View style={styles.container}>
-    <Text>{outputText}</Text>
-      <Button title ="Change Text" onPress={() => setOutputText('the text changed!')}/>
-    </View>
-  );
+const API_KEY = "353412764818359b0dc6ae189edf401e";
+
+export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
+  };
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude ) 
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+  _getWeather = (lat, long) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        temperature: json.main.temp,
+        name : json.weather[0].main
+      })
+    })
+  };
+  render() {
+    const { isLoaded, error } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather></Weather>
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
+  errorText: {
+    color: "red",
+    marginBottom: 40
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#FDF6AA",
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 24
+  }
 });
