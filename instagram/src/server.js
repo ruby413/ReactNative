21eed410cf13a6c9997421be559d1e4d377b29cd
juyeon_nmodelin <ruby413@hanmail.net>
2020-01-14 13:45:41 +0900

    change dotenv path setting

--- instagram/src/server.js
+++ instagram/src/server.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({path : path.resolve(__dirname,".env")})
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema"
-
 
 const PORT = process.env.PORT || 4000;
 
