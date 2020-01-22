3f3fd3b3aa5c5bd10995feb4ebe4f64179c79c34
juyeon_nmodelin <ruby413@hanmail.net>
2020-01-22 14:24:25 +0900

    can send mail

--- instagram/src/server.js
+++ instagram/src/server.js
@@ -1,17 +1,15 @@
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({path : path.resolve(__dirname,".env")})
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
-import schema from "./schema"
+import schema from "./schema";
+import { sendSecretMail } from "../utils";
+
 
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
 
-server.express.use(logger("dev"))
+server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on  http://localhost:${PORT}`)
 );
-
--- instagram/utils.js
+++ instagram/utils.js
@@ -1,6 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(`${__dirname}/src`, ".env") });
+console.log(__dirname);
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
 
-export const generateSecret = () =>{
-    const randomNumber = Math.floor(Math.random() * adjectives.length)
-    return `${adjectives[randomNumber]} ${nouns[randomNumber]}`
-}
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "ruby413@nmodelin.page",
+    to: adress,
+    subject: "login Secret",
+    html: `Hello! Your login secret it ${secret}. <br/> Copy paste on the app`
+  };
+  return sendMail(email);
+};
